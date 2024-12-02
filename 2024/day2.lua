e449a76e216b0e32f1e45c78459f5e1db9facd4d
DarkblooM-IO <darkbloompro@gmail.com>
2024-12-02 20:58:37 +0100

    completed 2024/day2 part 2

--- 2024/day2.lua
+++ 2024/day2.lua
@@ -2,27 +2,21 @@ package.path = package.path .. ";../?.lua"
 local utils = require "utils"
 
 function isIncr(tbl)
-  for i = 1, #tbl -1 do
+  for i = 1, #tbl - 1 do
     if tbl[i] > tbl[i + 1] then return false end
   end
   return true
 end
 
 function isDecr(tbl)
-  for i = 1, #tbl -1 do
+  for i = 1, #tbl - 1 do
     if tbl[i] < tbl[i + 1] then return false end
   end
   return true
 end
 
-local result = 0
-
-for i,v in ipairs(utils.lines) do
-  local report = utils.split(v)
+function isReportSafe(report)
   local safe = true
-
-  for x,y in ipairs(report) do report[x] = tonumber(y) end
-
   for n = 1, #report - 1 do
     local diff = math.abs(report[n] - report[n + 1])
     if diff < 1 or diff > 3 then
@@ -30,8 +24,36 @@ for i,v in ipairs(utils.lines) do
       break
     end
   end
-
-  if safe and (isIncr(report) or isDecr(report)) then result = result + 1 end
+  return safe
 end
 
-print(result)
+local safe_reports = {}
+local unsafe_reports = {}
+
+for i,v in ipairs(utils.lines) do
+  local report = utils.split(v)
+  for x,y in ipairs(report) do report[x] = tonumber(y) end
+
+  if isReportSafe(report) and (isIncr(report) or isDecr(report)) then
+    table.insert(safe_reports, report)
+  else
+    table.insert(unsafe_reports, report)
+  end
+end
+
+print("Part 1: "..#safe_reports)
+
+for _, report in ipairs(unsafe_reports) do
+  for i = 1, #report do
+    local copy = utils.copyTbl(report)
+
+    table.remove(copy, i)
+
+    if isReportSafe(copy) and (isIncr(copy) or isDecr(copy)) then
+      table.insert(safe_reports, report)
+      break
+    end
+  end
+end
+
+print("Part 2: "..#safe_reports)
