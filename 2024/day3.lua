9fe116aa7c5a1f9ffd587c7eb23c4e399e347d73
DarkblooM-IO <darkbloompro@gmail.com>
2024-12-03 11:34:13 +0100

    updated 2024/day3 part 2 (not working yet)

--- 2024/day3.lua
+++ 2024/day3.lua
@@ -2,7 +2,6 @@ package.path = package.path .. ";../?.lua"
 local utils = require "utils"
 
 local total1 = 0
-local total2 = 0
 
 for _, line in ipairs(utils.lines) do
   local mtch = utils.extractMatches(line, "mul%((%d+,%d+)%)")
@@ -20,3 +19,27 @@ for _, line in ipairs(utils.lines) do
 end
 
 print("Part 1: "..total1)
+
+local total2 = 0
+
+for _, line in ipairs(utils.lines) do
+  local statement = ""
+  local nums = {}
+  local mul = true
+
+  for chr in string.gmatch(line, ".") do
+    local mul_pattern = "^.*mul%((%d+,%d+)%)$"
+
+    statement = statement..chr
+
+    if string.match(statement, "do%(%)$") then
+      mul = true
+    elseif string.match(statement, "don't%(%)$") then
+      mul = false
+    elseif mul and string.match(statement, mul_pattern) then
+      local mtch = utils.extractMatches(statement, mul_pattern)
+      print(mtch[#mtch])
+      table.insert(nums, utils.split(mtch[#mtch], ","))
+    end
+  end
+end
