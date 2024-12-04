43461a631a01b73cd105d17c011adf5ee5bdb260
DarkblooM-IO <darkbloompro@gmail.com>
2024-12-04 15:20:33 +0100

    2024/day4 rewrite (still not working yet)

--- 2024/day4.lua
+++ 2024/day4.lua
@@ -1,6 +1,25 @@
 package.path = package.path .. ";../?.lua"
 local utils = require "utils"
 
+local total = 0
 
+for row = 1, #utils.lines do
+  for col = 1, #utils.lines[1] do
+    local substr = utils.lines[row]:sub(col, col + 3)
+    if substr == "XMAS" or substr == "SAMX" then total = total + 1 end
+  end
+end
+
+for col = 1, #utils.lines[1] do
+  for row = 1, #utils.lines do
+    local substr = utils.lines[row]:sub(col, col)
+
+    for i = 1, 3 do
+      if row + i <= #utils.lines then substr = substr..utils.lines[row + i]:sub(col, col) end
+    end
+    
+    if substr == "XMAS" or substr == "SAMX" then total = total + 1 end
+  end
+end
 
 print(total)
