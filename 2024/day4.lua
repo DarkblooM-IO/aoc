9f66022f748e677bb1d1638965b8b2f087b782e3
DarkblooM-IO <darkbloompro@gmail.com>
2024-12-04 11:09:21 +0100

    added vertical findings in 2024/day4 part 1

--- 2024/day4.lua
+++ 2024/day4.lua
@@ -1,10 +1,18 @@
 package.path = package.path .. ";../?.lua"
 local utils = require "utils"
 
+local function xmas(str) return #utils.extractMatches(str, "XMAS") + #utils.extractMatches(str, "SAMX") end
+
 local total = 0
 
 for _, line in ipairs(utils.lines) do
-  total = total + #utils.extractMatches(line, "XMAS") + #utils.extractMatches(line, "SAMX")
+  total = total + xmas(line)
+end
+
+for i = 1, #utils.lines[1] do
+  local vline = ""
+  for k = 1, #utils.lines do vline = vline..string.sub(utils.lines[k], i, i) end
+  total = total + xmas(vline)
 end
 
 print(total)
