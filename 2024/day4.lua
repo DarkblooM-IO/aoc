7a1db6592d698932932fc5cc3d3b4787497e5c25
DarkblooM-IO <darkbloompro@gmail.com>
2024-12-04 16:52:13 +0100

    2024/day4 reset

--- 2024/day4.lua
+++ 2024/day4.lua
@@ -5,28 +5,4 @@ local function xmas(str) return str == "XMAS" or str == "SAMX" end
 
 local total = 0
 
--- horizontal
-for row = 1, #utils.lines do
-  for col = 1, #utils.lines[1] do
-    local substr = utils.lines[row]:sub(col, col + 3)
-    if xmas(substr) then total = total + 1 end
-  end
-end
-
--- vertical
-for col = 1, #utils.lines[1] do
-  for row = 1, #utils.lines do
-    local substr = utils.lines[row]:sub(col, col)
-
-    for i = 1, 3 do
-      if row + i <= #utils.lines then substr = substr..utils.lines[row + i]:sub(col, col) end
-    end
-    
-    if xmas(substr) then total = total + 1 end
-  end
-end
-
--- diagonal
-
-
 print(total)
