9e82d130bd2f7f595577eb5b3ebcaf10fbe94fa2
DarkblooM-IO <darkbloompro@gmail.com>
2024-12-04 14:02:06 +0100

    2024/day4 reset

--- 2024/day4.lua
+++ 2024/day4.lua
@@ -1,23 +1,6 @@
 package.path = package.path .. ";../?.lua"
 local utils = require "utils"
 
-local function xmas(str) return #utils.extractMatches(str, "XMAS") + #utils.extractMatches(str, "SAMX") end
-
-local total = 0
-
--- horizontal
-for i,v in ipairs(utils.lines) do
-  total = total + xmas(v)
-end
-
--- vertical
-for i = 1, #utils.lines[1] do
-  local line = ""
-  for k = 1, #utils.lines do line = line..utils.lines[k]:sub(i, i) end
-  total = total + xmas(line)
-end
-
--- diagonal
 
 
 print(total)
