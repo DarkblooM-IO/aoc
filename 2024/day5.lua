de70241dac8ddeca364060d4d53cc7e509471615
DarkblooM-IO <darkbloompro@gmail.com>
2024-12-05 13:51:07 +0100

    commit

--- 2024/day5.lua
+++ 2024/day5.lua
@@ -19,4 +19,20 @@ for _, line in ipairs(utils.lines) do
   end
 end
 
+for _, pages in ipairs(page_updates) do
+  local valid = true
+  print(pages[#pages - ((#pages - 1) / 2)])
+  for i = 1, #pages - 1 do
+    for j = 1, #pages do
+      local test = {pages[i], pages[j]}
+      if utils.tblInTbl(page_orders, test) then
+        valid = true
+        -- print(table.concat(test, ";"))
+        break
+      else valid = false end
+    end
+  end
+  if valid then total = total + 1 end
+end
+
 print(total)
